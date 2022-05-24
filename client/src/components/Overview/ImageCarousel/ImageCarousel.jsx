9f1a6e95df6c48397d7176e80b782071f75c0197
CamEstep <camestep8517@gmail.com>
2022-05-24 13:13:11 -0400

    Progress on Image Carousel

--- client/src/components/Overview/ImageCarousel/ImageCarousel.jsx
+++ client/src/components/Overview/ImageCarousel/ImageCarousel.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ImageCarouselData from './ImageCarouselData.jsx';
 
 const ImageCarousel = (props) => {
+  const [current, setCurrent] = useState(0);
+  //const length = slides.length;
+
   return (
     <div>
-      <div class='bg-gray-700 max-h-full w-8/12 h-screen'>
-        Test
-      </div>
+        {ImageCarouselData.map((slide, index) => {
+          return (
+            <img src={slide.image} key={slide.id} alt='random image' />
+          )
+        })}
     </div>
   )
 };
