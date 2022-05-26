dbbe9ffb7953fb6ede2591b20d051f785d7bd33b
Philip Koller <philipkoller@Philips-MacBook-Pro.local>
2022-05-26 10:50:26 -0500

    seperated carousel data

--- client/src/components/Similar-Products/Carousel.jsx
+++ client/src/components/Similar-Products/Carousel.jsx
@@ -1,61 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Card from "./Card.jsx";
 import axios from "axios";
+import { CardsContext } from './Similar-Products.jsx';
 
 
-export const CardsContext = React.createContext();
 
 export default function Carousel() {
-  const [cards, setCards] = useState(
-    [
-      { id: 1, name: "coolpants", category: "pants", price: "140.00", url: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg", active: true },
-      { id: 2, name: "coolhat", category: "hats", price: "140.00", url: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg", active: true },
-      { id: 3, name: "coolshirt", category: "shirts", price: "140.00", url: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg", active: true },
-      { id: 4, name: "coolshoes", category: "Jackets", price: "140.00", url: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg", active: true },
-      { id: 5, name: "coolbill", category: "Jackets", price: "140.00", url: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg", active: false },
-      { id: 6, name: "coolrunners", category: "Jackets", price: "140.00", url: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg", active: false },
-      { id: 7, name: "coolluke", category: "Jackets", price: "140.00", url: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg", active: false },
 
-    ]
-  );
-
-  const getRelatedProductsAndStore = async () => {
-    const { data } = await axios.get('/api/related', { params: { product_id: '37314' } });
-
-    let array = [];
-    let i = 1;
-    for (let id of data) {
-      let card = {};
-      let product = await axios.get('/api/product', { params: { product_id: id } });
-      card["id"] = i;
-      card["name"] = product.data.name;
-      card["category"] = product.data.category;
-      card["price"] = product.data.default_price;
-
-      let productStyle = await axios.get('/api/product/style', { params: { product_id: id } });
-      if (productStyle.data.results[0].photos[0].url !== null) {
-        card["url"] = productStyle.data.results[0].photos[0].url
-      } else {
-        card["url"] = "https://www.ncenet.com/wp-content/uploads/2020/04/No-image-found.jpg"
-      }
-      if (i > 4) {
-        card["active"] = false;
-      } else {
-        card["active"] = true;
-      }
-      array.push(card);
-      i++;
-    }
-
-    setCards([...array]);
-
-  };
-
-  useEffect(() => {
-    getRelatedProductsAndStore();
-  }, []);
-
-
+  const card = useContext(CardsContext);
 
   const handleLeftClick = (isLeft) => {
 
@@ -136,7 +88,6 @@ export default function Carousel() {
                   </CardsContext.Provider>)
               })
             }
-
           </div>
         </div>
       </div>
