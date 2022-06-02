2c54f02fb793ddfc8a876b138237789a010b420d
CamEstep <camestep8517@gmail.com>
2022-06-02 18:48:27 -0400

    new version

--- client/src/components/Overview/ImageCarousel/ImageCarousel.jsx
+++ client/src/components/Overview/ImageCarousel/ImageCarousel.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import ThumbnailCarousel from './ThumbnailCarousel.jsx';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
-const axios = require('axios');
+
 
 const ImageCarousel = ({ activeStyle }) => {
   //Data Check
@@ -10,6 +11,9 @@ const ImageCarousel = ({ activeStyle }) => {
 
   const photoUrl = activeStyle[0].photos.map(photo => {
     return photo.url;
+  });
+  const thumbnailUrl = activeStyle[0].photos.map(photo => {
+    return photo.thumbnail_url;
   });
 
   // State Hooks
@@ -31,26 +35,33 @@ const ImageCarousel = ({ activeStyle }) => {
     return null
   }
 
-  //  Carousel Construct
+  // Carousel Construct
   return (
-    <section className='relative flex flex-col justify-center max-h-full overflow-hidden'>
-      <div className="flex flex-col justify-center max-h-3/4 w-full overflow-hidden object-cover object-bottom">
-        <div className='absolute top-1/2 left-8 z-10 cursor-pointer select-none text-5xl'>
-            <FaArrowAltCircleLeft onClick={prevSlide}/>
+    <div>
+      <div>
+      <section className='relative flex flex-col justify-center max-h-full overflow-hidden'>
+        <div className="flex flex-col justify-center max-h-3/4 w-full overflow-hidden object-cover object-bottom">
+          <div className='absolute top-1/2 left-8 z-10 cursor-pointer select-none text-5xl'>
+              <FaArrowAltCircleLeft onClick={prevSlide}/>
+          </div>
+          <div className='absolute top-1/2 right-8 z-10 cursor-pointer select-none text-5xl'>
+              <FaArrowAltCircleRight onClick={nextSlide}/>
+          </div>
+          {photoUrl.map((image, index) => {
+            return (
+              <>
+                  {index === current && (<img key={index} src={image} alt='random image'
+                  className='-z-5 rounded-md'/>)}
+              </>
+            )
+          })}
         </div>
-        <div className='absolute top-1/2 right-8 z-10 cursor-pointer select-none text-5xl'>
-            <FaArrowAltCircleRight onClick={nextSlide}/>
+      </section>
         </div>
-        {photoUrl.map((image, index) => {
-          return (
-            <>
-                {index === current && (<img key={index} src={image} alt='random image'
-                className='-z-5 rounded-md'/>)}
-            </>
-          )
-        })}
+      <div>
+        <ThumbnailCarousel thumbnailUrl={thumbnailUrl}/>
       </div>
-    </section>
+    </div>
   )
 };
 
