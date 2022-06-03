56c0402ff7cbebd3e98f474274e145e1fc9d316c
CamEstep <camestep8517@gmail.com>
2022-06-02 20:04:23 -0400

    Css and horizontal carousel

--- client/src/components/Overview/ImageCarousel/ImageCarousel.jsx
+++ client/src/components/Overview/ImageCarousel/ImageCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ThumbnailCarousel from './ThumbnailCarousel.jsx';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
@@ -39,25 +39,28 @@ const ImageCarousel = ({ activeStyle }) => {
   return (
     <div>
       <div>
-      <section className='relative flex flex-col justify-center max-h-full overflow-hidden'>
-        <div className="flex flex-col justify-center max-h-3/4 w-full overflow-hidden object-cover object-bottom">
-          <div className='absolute top-1/2 left-8 z-10 cursor-pointer select-none text-5xl'>
-              <FaArrowAltCircleLeft onClick={prevSlide}/>
+        <section className='relative flex flex-col justify-center max-h-full overflow-hidden'>
+
+          <div className="flex flex-col justify-center max-h-3/4 w-full overflow-hidden object-cover object-bottom">
+
+            <div className='absolute top-1/2 left-8 z-10 cursor-pointer select-none text-5xl'>
+                <FaArrowAltCircleLeft onClick={prevSlide}/>
+            </div>
+
+            <div className='absolute top-1/2 right-8 z-10 cursor-pointer select-none text-5xl'>
+                <FaArrowAltCircleRight onClick={nextSlide}/>
+            </div>
+            {photoUrl.map((image, index) => {
+              return (
+                <div key={index} className='h-1/2'>
+                    {index === current && (<img src={image} alt='random image'
+                    className='-z-5 rounded-md' />)}
+                </div>
+              )
+            })}
           </div>
-          <div className='absolute top-1/2 right-8 z-10 cursor-pointer select-none text-5xl'>
-              <FaArrowAltCircleRight onClick={nextSlide}/>
-          </div>
-          {photoUrl.map((image, index) => {
-            return (
-              <>
-                  {index === current && (<img key={index} src={image} alt='random image'
-                  className='-z-5 rounded-md'/>)}
-              </>
-            )
-          })}
-        </div>
-      </section>
-        </div>
+        </section>
+      </div>
       <div>
         <ThumbnailCarousel thumbnailUrl={thumbnailUrl}/>
       </div>
